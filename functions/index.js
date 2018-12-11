7b3f7135b8581e9915449424b669154c2fcd50e4
David Hayes <drhayes@gmail.com>
2018-12-11 13:56:58 -0600

    Add function to look at my email.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
+admin.initializeApp();
+
+exports.getStarredEmail = functions.https.onRequest((request, response) => {
+  console.log(request.body.body);
+  response.send(200);
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
