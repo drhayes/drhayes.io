6e76c237d5bb528db024a365fc2625daa5006c26
David Hayes <drhayes@gmail.com>
2021-05-06 16:42:23 -0500

    Works, but is suboptimal.
    Thinking about getting rid of the "index.mdx" pages in the src directory
    and turning them into "blaster.mdx", "gemini-rising.mdx", etc.

--- lib/components/gameShowcase.tsx
+++ lib/components/gameShowcase.tsx
@@ -4,7 +4,7 @@ import styles from './gameShowcase.module.css';
 
 export default function GameShowcase({ game }: { game: GameInfo }) {
   return (
-    <Link href={`${game.slug}/`}>
+    <Link href={`${game.slug}`}>
       <a className={styles.container}>
         <section className={styles.innerContainer} style={{ backgroundImage: `url('${game.screenshotPath}` }}>
           <h1 className={styles.title}>{game.name}</h1>
---
+++ pages/games/[game]/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import { getPage, getGames } from '../../../lib/pages';
+import MarkdownPage from '../../../lib/components/markdownPage';
+
+export default function GameIndex({ page }) {
+  return <MarkdownPage page={page} />;
+}
+
+export const getStaticProps: GetStaticProps = async ({ params: { game } }) => {
+  const page = await getPage(`/games/${game}/index`);
+  const renderedPage = await page.render();
+  return {
+    props: {
+      page: renderedPage,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allGames = await getGames();
+  const gameSlugs = allGames.map(({ slug }) => {
+    // Assumes all games are onder "/games/thing/". If that's not the case, this'll break.
+    const modifiedSlug = slug.replace(/\/games\//, '').replace(/\/$/i, '');
+    return {
+      params: {
+        game: modifiedSlug,
+      },
+    };
+  });
+  return {
+    paths: gameSlugs,
+    fallback: false,
+  };
+};
