3edbabdc883bfc02a46fda701577de03f07026f5
David Hayes <drhayes@gmail.com>
2021-06-15 15:15:16 -0500

    Hey ho SvelteKit.

---
+++ src/lib/siteMenu.svelte
@@ -0,0 +1,28 @@
+<script>
+  const menuItems = [
+    { "name": "Now", "url": "/now" },
+    { "name": "Lately", "url": "/lately" },
+    { "name": "Notes", "url": "https://notes.drhayes.io" },
+    { "name": "Blog", "url": "/blog" },
+    { "name": "Games", "url": "/games" },
+    { "name": "About", "url": "/about" }
+  ];
+</script>
+
+<style>
+  ol {
+    display: flex;
+  }
+
+  li {
+    margin-right: 1rem;
+  }
+</style>
+
+<ol>
+  {#each menuItems as item}
+    <li>
+      <a href="{item.url}">{item.name}</a>
+    </li>
+  {/each}
+</ol>
---
+++ src/routes/index.svelte
@@ -0,0 +1,55 @@
+<script>
+  import RavenLogo from '$lib/ravenLogo.svelte';
+  import SiteMenu from '$lib/siteMenu.svelte';
+</script>
+
+<style>
+  .verticalCenter {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 60rem;
+    height: 100vh;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .horizontalCenter {
+    display: flex;
+  }
+
+  .left {
+    width: 30ch;
+  }
+
+  .right {
+    /* flex: 1; */
+  }
+
+  h1 {
+    font-size: 300%;
+    font-weight: bold;
+  }
+
+  h2 {
+    font-size: 150%;
+  }
+</style>
+
+<section class="verticalCenter">
+  <div class="horizontalCenter">
+    <div class="left">
+      <RavenLogo />
+    </div>
+    <div class="right">
+      <h1>Hi. 👋🏽</h1>
+      <h2>My name is David Hayes.</h2>
+      <h2><a href="mailto:hi@drhayes.io">hi@drhayes.io</a></h2>
+      <h2>I do stuff with computers.</h2>
+    </div>
+  </div>
+  <div class="horizontalCenter">
+    <SiteMenu />
+  </div>
+</section>
