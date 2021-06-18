a805cf14a9c7e0e6f653af512d0550f7e26c1750
David Hayes <drhayes@gmail.com>
2021-06-18 08:57:24 -0500

    Added an error page.

---
+++ src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="typescript">
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`,
+      }
+    }
+  }
+</script>
+
+<script>
+  import DefaultLayout from '../layouts/default.svelte';
+  export let title;
+</script>
+
+<DefaultLayout title={title}>
+  <p>The sadness!</p>
+  <p>You should try some other site content like:</p>
+  <ul>
+    <li>
+      <a href="/about">The about page.</a>
+    </li>
+    <li>
+      <a href="/games">The games page.</a>
+    </li>
+  </ul>
+</DefaultLayout>
