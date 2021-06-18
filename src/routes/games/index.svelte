7c5174207a6a02888856939c1e3beb6cd4926d80
David Hayes <drhayes@gmail.com>
2021-06-18 13:16:03 -0500

    Boxed the game listing.

--- src/routes/games/index.svelte
+++ src/routes/games/index.svelte
@@ -16,9 +16,25 @@
   export let games;
 </script>
 
-<DefaultLayout title="Games">
-  {#each games as game}
-    <GameTile title={game.title} slug={game.slug} screenshotUrl={game.screenshot} />
-  {/each}
+<style>
+  ol {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
 
+  li {
+    border: 1px dotted var(--light-border-color);
+    margin-bottom: 1rem;
+  }
+</style>
+
+<DefaultLayout title="Games">
+  <ol>
+  {#each games as game}
+    <li>
+      <GameTile title={game.title} slug={game.slug} screenshotUrl={game.screenshot} />
+    </li>
+  {/each}
+  </ol>
 </DefaultLayout>
