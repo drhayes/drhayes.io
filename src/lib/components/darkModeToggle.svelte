07e7d5b2b72754277f3ea26e1496ae5cc56790b1
David Hayes <drhayes@gmail.com>
2021-07-16 10:35:10 -0500

    Toggle control itself is okay now.

--- src/lib/components/darkModeToggle.svelte
+++ src/lib/components/darkModeToggle.svelte
@@ -1,7 +1,22 @@
+<style>
+  button {
+    border: 0;
+    color: var(--text-content-color);
+    background-color: transparent;
+    cursor: pointer;
+  }
+</style>
+
 <script lang="typescript">
-  // import SunIcon from './icons/sun.svelte';
-  // import MoonIcon from './icons/moon.svelte';
+  import SunIcon from './icons/sun.svelte';
+  import MoonIcon from './icons/moon.svelte';
   import userPreferences from '$stores/userPreferences';
 </script>
 
-<button on:click="{() => userPreferences.toggleDarkMode()}">Toggle</button>
+<button on:click="{() => userPreferences.toggleDarkMode()}">
+  {#if $userPreferences.isDarkMode}
+    <MoonIcon />
+  {:else}
+    <SunIcon />
+  {/if}
+</button>
