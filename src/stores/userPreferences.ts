2b39d0562191fca0c24274be334e0a9f33a5cf6b
David Hayes <drhayes@gmail.com>
2021-07-16 11:07:09 -0500

    User preferences store in localStorage in browser.

--- src/stores/userPreferences.ts
+++ src/stores/userPreferences.ts
@@ -1,12 +1,53 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
+import { browser } from '$app/env';
 
-const { subscribe, set, update } = writable({
+const STORAGE_KEY = 'userPreferences';
+
+// What are the defaults?
+let initialValue = {
   isDarkMode: true,
-});
+};
 
-subscribe(value => {
-  console.log(value);
-});
+const { subscribe, set, update } = writable(initialValue);
+
+// If we're in  a browser context, serialize to localStorage.
+if (browser) {
+  const readPreferencesFromStorage = () => {
+    const preferenceString = localStorage.getItem(STORAGE_KEY);
+    if (preferenceString) {
+      try {
+        return JSON.parse(preferenceString);
+      } catch (error) {
+        console.error('Error deserializing preferences from storage', error);
+      }
+    }
+  };
+
+  // Is there anything in localStorage? Use that.
+  const prefsFromStorage = readPreferencesFromStorage();
+  if (prefsFromStorage) {
+    initialValue = {
+      ...initialValue,
+      ...prefsFromStorage,
+    };
+  }
+
+  // If it changes, put it back in localStorage.
+  subscribe(value => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+  });
+
+  // Listen for storage events from other tabs/windows.
+  window.addEventListener('storage', () => {
+    const prefsFromStorage = readPreferencesFromStorage();
+    if (prefsFromStorage) {
+      set(prefsFromStorage);
+    }
+  });
+}
+
+// Set our initial value now that that's all settled.
+set(initialValue);
 
 const toggleDarkMode = (): void => {
   // Dark by default.
