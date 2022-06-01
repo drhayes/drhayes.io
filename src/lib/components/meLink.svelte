9a13849a074a67b5bdccc8b121b9849cfe4cda92
David Hayes <drhayes@hey.com>
2022-06-01 09:48:22 -0500

    Getting sitemap to work.

--- src/lib/components/meLink.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  export let href;
-</script>
-
-<a href={href} rel="me"><slot /></a>
--- src/lib/slugFromPath.ts
+++
@@ -1,3 +0,0 @@
-const slugFromPath = (path: string): string | null => path.match(/([\w-]+)\.(md)/i)?.[1] ?? null;
-
-export default slugFromPath;
