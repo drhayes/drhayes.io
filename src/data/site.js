b546604006e2f931715d70291cea79ea9d4a442b
David Hayes <drhayes@omg.lol>
2024-03-04 09:31:01 -0600

    Update bad robots list.

--- src/data/site.js
+++ src/data/site.js
@@ -1,32 +1,23 @@
 const isDev = require('./isDev');
 
 const siteData = {
-  "name": "drhayes.io",
-  "subtitle": "The personal site of David Hayes.",
-  "host": "https://drhayes.io",
-  "domain": "drhayes.io",
-  "language": "en",
-  "author": {
-    "name": "David Hayes",
-    "email": "hi@drhayes.io",
-    "link": "https://drhayes.io",
-    "avatar": "https://drhayes.io/img/raven.png"
+  'name': 'drhayes.io',
+  'subtitle': 'The personal site of David Hayes.',
+  'host': 'https://drhayes.io',
+  'domain': 'drhayes.io',
+  'language': 'en',
+  'author': {
+    'name': 'David Hayes',
+    'email': 'hi@drhayes.io',
+    'link': 'https://drhayes.io',
+    'avatar': 'https://drhayes.io/img/raven.png'
   },
-  "copyright": "This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.",
-  "googleSiteVerification": "62OprotnkXjdlu5isXeGX010S3smwpIK4aHKez_WBJU",
-  "badRobots": [
-    "ChatGPT-User",
-    "GPTBot",
-    "Googlebot",
-    "Googlebot-Image",
-    "bingbot",
-    "redditbot",
-    "yandex"
-  ]
+  'copyright': 'This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.',
+  'googleSiteVerification': '62OprotnkXjdlu5isXeGX010S3smwpIK4aHKez_WBJU',
 };
 
 if (isDev) {
-  siteData.host = "http://localhost:3000";
+  siteData.host = 'http://localhost:3000';
 }
 
 module.exports = siteData;
