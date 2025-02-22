05fa8b183f5c24103ba02fa84c840357bd15d984
David Hayes <drhayes@omg.lol>
2025-02-22 10:30:39 -0600

    Add inspector call on Adam's advice and logging request id.

--- src/data/latestStatus.js
+++ src/data/latestStatus.js
@@ -3,6 +3,17 @@ const fetch = require('@11ty/eleventy-fetch');
 async function latestStatus() {
   const statusUrl = 'https://api.omg.lol/address/drhayes/statuses/latest';
 
+  // Help me and Adam to figure out what's going on here.
+  try {
+    const requestIdResponse = await fetch('https://neatnik.net/inspector/', {
+      duration: '4h',
+      type: 'text',
+    });
+    console.log(`Request ID: ${requestIdResponse}`);
+  } catch (error) {
+    console.error('Error fetching request from inspector', error);
+  }
+
   try {
     const statusResponse = await fetch(statusUrl, {
       duration: '4h',
