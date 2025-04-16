054a1890c5ef1b2ae23918619b8daf6919e3ebc7
David Hayes <drhayes@omg.lol>
2025-04-16 11:31:47 -0500

    Don't open the editor, it's behaving weird in my terminal.
    Not sure if that's because I'm passing the wrong params for that stdio
    or what. I promise to fix it, cuz I want that functionality -- but it's
    annoying me right now.

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -5,7 +5,6 @@ const slugify = require('@sindresorhus/slugify');
 const { join: joinPath, dirname } = require('path');
 const dateFns = require('date-fns');
 const { writeFileSync, mkdirSync } = require('fs');
-const { spawn } = require('child_process');
 
 const program = new Command();
 
@@ -31,13 +30,6 @@ program
     const template = linkTemplate({ link, title, formattedDate });
     mkdirSync(dirname(path), { recursive: true });
     writeFileSync(path, template);
-    const editor = process.env.EDITOR;
-    if (editor) {
-      spawn(editor, [path], {
-        stdio: 'inherit',
-        detached: true,
-      });
-    }
   });
 
 function linkTemplate({ link, title, formattedDate }) {
